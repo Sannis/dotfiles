3176469cd9d814d9d94454719703f5d8fbeb867f
Oleg Efimov <efimovov@gmail.com>
2016-12-26 20:53:58 +0300

    Aggresively git prune

--- bin/git_prune_and_gc.sh
+++ bin/git_prune_and_gc.sh
@@ -1,9 +1,20 @@
 #!/bin/sh
 
 # http://noteskeeper.ru/621/
+# https://git-scm.com/docs/git-reflog
+# http://stevelorek.com/how-to-shrink-a-git-repository.html
+
+du -sh .
 
 git remote prune origin
+du -sh .
 
-#git reflog expire --expire=1.day refs/heads/master
+git reflog expire --expire-unreachable refs/heads/master
+du -sh .
+
 git fsck --unreachable
-git gc
+du -sh .
+
+git gc --aggressive --prune=now
+du -sh .
+
